da0066f8da43b9d29e5df3cae2eb22fae6130e63
André Loureiro <contato.loureiro1@gmail.com>
2024-10-21 17:00:41 -0300

    Componente Button

--- App.tsx
+++ App.tsx
@@ -1,5 +1,30 @@
+import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
 import { SignIn } from "./src/screens/SignIn";
+import theme from "./src/theme";
+import { Loading } from "./src/components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
-  return <SignIn />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
+      <SignIn />
+    </ThemeProvider>
+  );
 }
